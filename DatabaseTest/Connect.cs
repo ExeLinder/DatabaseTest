e91370731c58f8bb3be9634fa1b2885f1c3b58c5
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2025-05-21 13:44:15 +0300

    Add project files.

---
+++ DatabaseTest/Connect.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DatabaseTest
+{
+    partial class DataBase
+    {
+        SqliteConnection connection;
+        public DataBase()
+        {
+            connection = new SqliteConnection("Data Source = people.db");
+
+            connection.Open();
+        }
+    }
+}
