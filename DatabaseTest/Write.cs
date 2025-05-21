faa91e48d270e2d87988c46a1bb3cc0348c4b6df
DenisTrigub <denistrigub66@gmail.com>
2025-05-21 14:24:55 +0300

    added Write method

---
+++ DatabaseTest/Write.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DatabaseTest
+{
+    partial class DataBase
+    {
+        public void Write(string name, int age)
+        {
+            string sql = $"insert into person (name, age) values ('{name}', {age})";
+            SqliteCommand command = new SqliteCommand(sql, connection);
+            command.ExecuteNonQuery();
+            Console.WriteLine("запись успешно добавлена");
+        }
+    }
+}
