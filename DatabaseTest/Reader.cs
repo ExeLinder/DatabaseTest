7e2f3bf599a17346e386b55150d8c773f28f5158
CKa3o4HuK <71247156+N3minator@users.noreply.github.com>
2025-05-22 11:01:11 +0100

    Add Reader.cs

---
+++ DatabaseTest/Reader.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest
+{
+    partial class DataBase
+    {
+        public void Read()
+        {
+            string sql = "SELECT name, age FROM person";
+
+            SqliteCommand command = new SqliteCommand(sql, connection);
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                Console.WriteLine("Данные из таблицы 'person':\n");
+
+                while (reader.Read())
+                {
+                    string name = reader.GetString(0); // первая колонка — name
+                    int age = reader.GetInt32(1);       // вторая колонка — age
+
+                    Console.WriteLine($"Имя: {name}, Возраст: {age}\n");
+                }
+            }
+        }
+    }
+}
